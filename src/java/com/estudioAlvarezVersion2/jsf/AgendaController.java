809ce1f866cdce810fd41ef9df08bf84b706f096
109655-2W1-NievaAlexis <maxynieva27@gmail.com>
2025-09-12 02:39:20 -1200

    Subo nuevos ajustes en front expedientes judiciales front

--- src/java/com/estudioAlvarezVersion2/jsf/AgendaController.java
+++ src/java/com/estudioAlvarezVersion2/jsf/AgendaController.java
@@ -38,6 +38,7 @@ import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.EJBException;
@@ -156,7 +157,7 @@ public class AgendaController implements Serializable {
     }
 
     public List<String> getLideres() {
-        return lideresEmpleadosMap.keySet().stream().toList();
+        return lideresEmpleadosMap.keySet().stream().collect(Collectors.toList()); //este metodo lo cambio en mi local ya que me tira error
     }
     
     
